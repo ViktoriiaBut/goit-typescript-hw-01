9b3ffeb67549e872d7d615e62bca5ce712f69a31
ViktoriiaBut <zironka.tory@gmail.com>
2025-05-21 22:59:03 +0100

    1-5

---
+++ src/basic/3.ts
@@ -0,0 +1,7 @@
+let value: string | number;
+value = "Hi";  
+value = 42;
+
+let able: 'enable'| 'disable';
+able = 'disable';
+able = 'enable';
---
+++ src/basic/5.ts
@@ -0,0 +1,19 @@
+
+enum DayOfWeek {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+
+const isWeekend = (day: number): boolean => {
+    if (day === DayOfWeek.Saturday || day === DayOfWeek.Sunday) return true;
+    return false;
+}
+
+isWeekend(DayOfWeek.Friday);
+isWeekend(DayOfWeek.Saturday);
